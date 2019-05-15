c354ae718af7257237b98c5755b173f457ecc774
Sathish Shanmugam <sathish.shanmugam@chimp.net>
2019-05-15 18:10:27 +0530

    added code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("****************** hello boss this is from worker");
+
+var path = require("path");
+
+// Normalization
+console.log('normalization : ' + path.normalize('/test/test1//2slashes/1slash/tab/..'));
+
+// Join
+console.log('joint path : ' + path.join('/test', 'test1', '2slashes/1slash', 'tab', '..'));
+
+// Resolve
+console.log('resolve : ' + path.resolve('main.js'));
+
+// extName
+console.log('ext name : ' + path.extname('main.js'));
+
+process.exit(0);
